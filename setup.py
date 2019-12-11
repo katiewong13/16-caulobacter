39a11a48b73214954f5d5002ebebadd124fb4f48
katiewong <kwong2@caltech.edu>
2019-12-11 15:33:24 -0800

    fixed setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 
-with open("16-caulobacter/__init__.py", "r") as f:
+with open("16-caulobacter-dir/__init__.py", "r") as f:
     init = f.readlines()
 
 for line in init:
