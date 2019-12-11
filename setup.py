ca9263ff2f2f39ade0cc394043fb987799197d45
katiewong <kwong2@caltech.edu>
2019-12-11 15:13:17 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+with open("16-caulobacter/__init__.py", "r") as f:
+    init = f.readlines()
+
+for line in init:
+    if '__author__' in line:
+        __author__ = line.split("'")[-2]
+    if '__email__' in line:
+        __email__ = line.split("'")[-2]
+    if '__version__' in line:
+        __version__ = line.split("'")[-2]
+
+
+setup(
+    name='16-caulobacter',
+    version=__version__,
+    author=__author__,
+    author_email=__email__,
+    description='A package for Caulobacter growth (9.3a).',
+    long_description=long_description,
+    long_description_content_type='ext/markdown',
+    packages=find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+)
